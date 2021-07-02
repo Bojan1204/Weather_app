85d5b5a1bc2bc9487ff3c68a957b27d938e55920
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-07-02 19:01:20 +0200

    added api

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,27 @@
-// Custom JS
+const inputText = document.querySelector('.inputText');
+const button = document.querySelector('.btn')
+const showData = document.querySelector('.showData')
+
+const apiKey = "f5033f35557c18d54f4091754b9241d8";
+
+button.addEventListener('click', ()=>{
+
+    const cityInput = inputText.value;
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
+    .then(response=> response.json())
+    .then(data=>{
+        console.log(data)
+        showData.innerHTML = `
+        
+                <ul>
+                    <li class="desc">${data.weather[0].description}</li>
+                    <li class="city">${data.name}</li>
+                    <li class="temp">${data.main.temp}°C</li>
+                </ul>
+        
+        `
 
 
-// alert(`Hello`);
+    })
 
-
-
-
+})
