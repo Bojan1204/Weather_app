b15fc26e4805f0255b09711d961ce813021166aa
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-07-05 13:04:44 +0200

    converted kelvin to celsius

--- src/js/main.js
+++ src/js/main.js
@@ -10,15 +10,15 @@ button.addEventListener('click', ()=>{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
     .then(response=> response.json())
     .then(data=>{
-        // console.log(data)
+        console.log(data)
         const icon = data.weather[0].icon; 
-
+        let c = Math.floor(data.main.temp - 273.15);
         showData.innerHTML = `
         
                 <ul>
                     <li class="desc">${data.weather[0].description}</li>
                     <li class="city">${data.name}</li>
-                    <li class="temp">${data.main.temp}°C</li>
+                    <li class="temp">${c}°C</li>
                     <li class="temp"><img class= "weather-icon" src= "https://openweathermap.org/img/w/${icon}.png"/></li>
                     
                 </ul>
